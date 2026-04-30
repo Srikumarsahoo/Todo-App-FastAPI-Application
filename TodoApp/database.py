505413f169109aed2b4dd7dd5fc326c512fbecca
Srikumar <srikumarsgetspace@gmail.com>
2026-04-30 22:52:41 +0530

    Fixed render issue

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL =  'postgresql://postgres:test1234@localhost/postgres'
+SQLALCHEMY_DATABASE_URL =  'postgresql://postgres_database_7qam_user:saRy9PPKzERQk0yQfiVNb6hIQlfJvQw4@dpg-d7potm8sfn5c73aalkog-a/postgres_database_7qam'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -11,3 +11,7 @@ Base = declarative_base()
 
 # Sqlite3 path:
 # 'sqlite:///./todosapp.db'
+
+##postgresql://postgres_database_7qam_user:saRy9PPKzERQk0yQfiVNb6hIQlfJvQw4@dpg-d7potm8sfn5c73aalkog-a/postgres_database_7qam
+
+##postgresql://postgres:test1234@localhost/postgres
